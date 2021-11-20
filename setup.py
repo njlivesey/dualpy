06cfe75e197c00b307f76b070ccb0748d65c9430
Nathaniel J. Livesey <Nathaniel.J.Livesey@jpl.nasa.gov>
2021-11-19 21:46:30 -0800

    Bug fix on package

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
+
 # List of requirements
 requirements = []  # This could be retrieved from requirements.txt
 # Package (minimal) configuration
 setup(
     name="dualpy",
     version="1.0.0",
-    description="Dual algebra for automatic differentiation (building on astropy.units)"
+    description="Dual algebra for automatic differentiation (building on astropy.units)",
     packages=find_packages(),  # __init__.py folders search
-    install_requires=requirements
+    install_requires=requirements,
 )
-
