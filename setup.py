2b9048cdb9a6f193c77e995bf049f191fd1670db
Nathaniel J. Livesey <Nathaniel.J.Livesey@jpl.nasa.gov>
2025-06-07 20:33:59 -0700

    Rearranged for new packaging

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
 
-# List of requirements
-requirements = []  # This could be retrieved from requirements.txt
-# Package (minimal) configuration
 setup(
     name="dualpy",
     version="1.0.0",
-    description="Dual algebra for automatic differentiation (building on astropy.units)",
-    packages=find_packages(),  # __init__.py folders search
-    install_requires=requirements,
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "astropy",
+        "pint",
+        "numpy",
+        "scipy",
+        "sparse",
+    ],
+    python_requires=">=3.12",
 )
